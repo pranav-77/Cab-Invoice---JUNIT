209aebab622d5334fa3054b42d9e30d07c26799f
pranav-77 <pranavshazz77@gmail.com>
2024-11-04 12:02:56 +0530

    Added function to Enhanced Invoice

--- src/test/java/com/junit/InvoiceGeneratorTest.java
+++ src/test/java/com/junit/InvoiceGeneratorTest.java
@@ -7,7 +7,7 @@ public class InvoiceGeneratorTest {
     InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 
     @Test
-    public void ReturnTotalFare() {
+    public void CheckTotalFare() {
         double distance = 5;
         double time = 20;
         int expected = 70;
@@ -22,7 +22,7 @@ public class InvoiceGeneratorTest {
     }
 
     @Test
-    public void ReturnTotalFare_ForMultipleRides() {
+    public void CheckTotalFareForMultipleRides() {
         Ride[] rides = {
                 new Ride(5.0, 20.0),
                 new Ride(3.0, 10.0),
@@ -30,6 +30,22 @@ public class InvoiceGeneratorTest {
         };
         int expected = 5 * 10 + 20 * 1 + 3 * 10 + 10 * 1 + 5;
         int actual = (int) invoiceGenerator.calculateFareForMultipleRides(rides);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CheckEnhancedInvoice() {
+        Ride[] rides = {
+                new Ride(5.0, 20.0),
+                new Ride(3.0, 10.0),
+                new Ride(0.5, 0.2)
+        };
+        double expectedTotalFare = invoiceGenerator.calculateFare(5, 20) +
+                invoiceGenerator.calculateFare(3, 10) +
+                invoiceGenerator.calculateFare(0.5, 0.2);
+
+        InvoiceSummary expected = new InvoiceSummary(3, expectedTotalFare);
+        InvoiceSummary actual = invoiceGenerator.calculateEnhancedInvoice(rides);
         Assert.assertEquals(expected, actual);
     }
 }
